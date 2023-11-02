fcc3b9f3833b846b462fc39ac9fa98bd3d22c6d7
raihanrr11 <raihanrr400@gmail.com>
2023-11-02 09:12:39 +0800

    tambahan

--- AutocompleteAndTemplates/src/Main.java
+++ AutocompleteAndTemplates/src/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
     public static void main(String[] args) {
         // Masukkan nilai kecepatan (dalam m/s) dan waktu (dalam detik)
@@ -6,10 +7,11 @@ public class Main {
 
         // Hitung jarak
         double jarakMeter = kecepatan * waktu;
-
+        double jarakKilometer = jarakMeter / 1000;
         // Tampilkan hasil perhitungan
         System.out.println("Kecepatan: " + kecepatan + " m/s");
         System.out.println("Waktu: " + waktu + " detik");
         System.out.println("Jarak: " + jarakMeter + " meter");
+        System.out.println("Kilometer: " + jarakKilometer + "Kilometer");
     }
 }
